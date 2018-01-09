580275e5cd80e67f74474e6fb1839fa59711d71d
james <jamesdaniel2197@gmail.com>
2018-01-09 18:12:46 +0000

    remove the need for a palette lookup texture, use color vectors instead

--- src/webgl/fragmentShader.glsl.js
+++ src/webgl/fragmentShader.glsl.js
@@ -1,13 +1,14 @@
 export default [
   "precision mediump float;",
   "varying vec2 v_texcoord;",
-  "uniform sampler2D u_palette;", 
-  "uniform sampler2D u_layer_1;",
-  "uniform sampler2D u_layer_2;",    
+  "uniform vec4 u_paperColor;", 
+  "uniform vec4 u_layer1Color;", 
+  "uniform vec4 u_layer2Color;", 
+  "uniform sampler2D u_layer1Bitmap;",
+  "uniform sampler2D u_layer2Bitmap;",    
   "void main() {",
-      "float layer1 = texture2D(u_layer_1, v_texcoord).a * 255.0;",
-      "float layer2 = texture2D(u_layer_2, v_texcoord).a * 255.0;",
-      "float pixel = (layer1 == 0.0) ? (layer2 == 0.0) ? 0.0 : 2.0 : 1.0;",
-      "gl_FragColor = texture2D(u_palette, vec2((pixel + 0.5) / 256.0, 0.5));",
+      "float layer1 = texture2D(u_layer1Bitmap, v_texcoord).a * 255.0;",
+      "float layer2 = texture2D(u_layer2Bitmap, v_texcoord).a * 255.0;",
+      "gl_FragColor = (layer1 == 0.0) ? (layer2 == 0.0) ? u_paperColor : u_layer2Color : u_layer1Color;",
   "}"
 ].join("\n");
--- src/webgl/webglCanvas.js
+++ src/webgl/webglCanvas.js
@@ -31,23 +31,12 @@ export default class webglCanvas {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
     gl.enableVertexAttribArray(0);
     gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
-    // create an RGB palette
-    this.palette = new Uint8Array(256 * 4);
-    gl.uniform1i(gl.getUniformLocation(program, "u_palette"), 0);
-    // create palette texture
+
+    gl.uniform1i(gl.getUniformLocation(program, "u_layer1Bitmap"), 0);
     gl.activeTexture(gl.TEXTURE0);
-    var paletteTex = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, paletteTex);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 256, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.palette);
-    gl.uniform1i(gl.getUniformLocation(program, "u_layer_1"), 1);
-    gl.activeTexture(gl.TEXTURE1);
     this._initBitmap();
-    gl.uniform1i(gl.getUniformLocation(program, "u_layer_2"), 2);    
-    gl.activeTexture(gl.TEXTURE2);
+    gl.uniform1i(gl.getUniformLocation(program, "u_layer2Bitmap"), 1);    
+    gl.activeTexture(gl.TEXTURE1);
     this._initBitmap();
   }
 
@@ -61,29 +50,21 @@ export default class webglCanvas {
     // gl.texImage2D(gl.TEXTURE_2D, 0, gl.ALPHA, 256, 192, 0, gl.ALPHA, gl.UNSIGNED_BYTE, pixelArray)
   }
 
+  setColor(color, value) {
+    this.gl.uniform4f(this.gl.getUniformLocation(this.program, color), value[0] / 255, value[1] / 255, value[2] / 255, 1);
+  }
+
   setPalette(colors) {
-    this.palette[0] = colors[0][0];
-    this.palette[1] = colors[0][1];
-    this.palette[2] = colors[0][2];
-    this.palette[3] = 255;
-    this.palette[4] =  colors[1][0];
-    this.palette[5] =  colors[1][1];
-    this.palette[6] =  colors[1][2];
-    this.palette[7] = 255;
-    this.palette[8] =  colors[2][0];
-    this.palette[9] =  colors[2][1];
-    this.palette[10] =  colors[2][2];
-    this.palette[11] = 255;
-    var gl = this.gl;
-    gl.activeTexture(gl.TEXTURE0);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 256, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.palette);
+    this.setColor("u_paperColor", colors[0]);
+    this.setColor("u_layer1Color", colors[1]);
+    this.setColor("u_layer2Color", colors[2]);
   }
 
   setBitmaps(bitmaps) {
     var gl = this.gl;
-    gl.activeTexture(gl.TEXTURE1);
+    gl.activeTexture(gl.TEXTURE0);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.ALPHA, 256, 192, 0, gl.ALPHA, gl.UNSIGNED_BYTE, bitmaps[0]);
-    gl.activeTexture(gl.TEXTURE2);
+    gl.activeTexture(gl.TEXTURE1);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.ALPHA, 256, 192, 0, gl.ALPHA, gl.UNSIGNED_BYTE, bitmaps[1]);
   }
 
